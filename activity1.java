11ac83ca81e5767168a3cc4e6895e74c7a2871fa
JoanBobis-2I <jbbobis@paterostechnologicalcollege.edu.ph>
2025-02-22 02:42:39 +0800

    Added Name: Joan B. Bobis

--- activity1.java
+++ activity1.java
@@ -7,20 +7,13 @@ public class activity1 {
         //EXAMPLE:
 
         System.out.println("BENOYA, JHOVENSON D. - BSIT-2I");
-
-
         System.out.println("JHONNY B. MAPULA - BSIT-2J");
-
         System.out.println("NEIL JOHN DIAZ - BSIT 2I");
         System.out.println("MARC JOSH MARAVILLA - BSIT-2J");
         System.out.println("LHEIAN V. SAREN - BSIT-2J");
-
         System.out.println("VILLOSO JOMERSON M. - BSIT-2J");
-
         System.out.println("DARRYL YAM C. CANDILADA - BSIT-2I");
-
-
+        System.out.println("JOAN B. BOBIS - BSIT-2I");
 
     }
 }
-  
