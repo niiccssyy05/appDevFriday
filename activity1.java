000e638648d2078faf89eb5b850101441c46636c
juntabiosGit <165021843+juntabiosGit@users.noreply.github.com>
2025-02-28 16:07:06 +0800

    Update activity1.java FINAL COPY

--- activity1.java
+++ activity1.java
@@ -1,21 +1,14 @@
 public class Activity1 {
     public static void main(String[] args) {
+      // Collaborators: Add your names below using the format:
+      // System.out.println("Your Full Name - SECTION");
+      // System.out.println("Your Full Name - Section");
+      //EXAMPLE:
+        
         System.out.println("List of Students:");
-
         System.out.println("MARGARET MAEGAN T. ARMIGER - BSIT-2J");
-       
-         System.out.println("DEL ROSARIO, JOSHUA CEDRICK P. - BSIT-2J");
-
-        // Collaborators: Add your names below using the format:
-
-        // System.out.println("Your Full Name - SECTION");
-           
-           System.out.println("AGUSTIN GLENDY JR. P. - BSIT 2J")
-
-        // System.out.println("Your Full Name - Section");
-
-        //EXAMPLE:
-
+        System.out.println("DEL ROSARIO, JOSHUA CEDRICK P. - BSIT-2J");
+  System.out.println("AGUSTIN GLENDY JR. P. - BSIT 2J");  
         System.out.println("BENOYA, JHOVENSON D. - BSIT-2I");
         System.out.println("JHONNY B. MAPULA - BSIT-2J");
         System.out.println("NEIL JOHN DIAZ - BSIT 2I");
