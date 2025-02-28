8d71387bc2396503caae2c9067ef2f549ee3f271
mangaoang <ivanclard@gmail.com>
2025-02-28 20:29:03 +0800

    Added Mangaoang.java

---
+++ Mangaoang.java
@@ -0,0 +1,12 @@
+[PhotoshopCS6Portable]
+CheckRegWrite=true
+AllowMultipleInstances=false
+Language=
+Extended=true
+DisableSplashScreen=false
+SplashScreenName=
+public class Mangaoang {
+    public static void display() {
+        System.out.println("Hello! This is Cruz contributing to the project.");
+    }
+}
