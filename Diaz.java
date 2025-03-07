65d25b23969b11f833f08cde51fec4c09e867d45
Neil <nediaz@paterostechnologicalcollege.edu.ph>
2025-03-07 20:38:17 +0800

    Modified Diaz.java

--- Diaz.java
+++ Diaz.java
@@ -1,6 +1,6 @@
 public class Diaz {
     public static void display() {
-        int age = 19; // Replace with user input if necessary
+        int age = 19;
         System.out.println("Hello! This is Diaz contributing to the project.");
         System.out.println("My current age is: " + age);
         System.out.println("Next year, I will be: " + (age + 1));
