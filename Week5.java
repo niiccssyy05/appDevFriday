2755101503c65d3fcce45708e06a34889d65384e
juntabiosGit <165021843+juntabiosGit@users.noreply.github.com>
2025-03-07 19:24:22 +0800

    ORIGINAL FILE

---
+++ Week5.java
@@ -0,0 +1,8 @@
+public class Week5 {
+    public static void main(String[] args) {
+        System.out.println("Collaborative Java Project\n");
+
+        // Call each student's method
+        Cruz.display(); // Add your method call here
+    }
+}
