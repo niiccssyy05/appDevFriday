e735bf3f4d44be08f54ce56a4051b64a90dc98ec
Ac092102 <ajcangmaong@paterostechnologicalcollege.edu.ph>
2025-03-08 02:16:52 +0800

    Update Week5.java

--- Week5.java
+++ Week5.java
@@ -6,20 +6,7 @@ public class Week5 {
         // Call each student's method
         // Add your method below
         // Cruz.display(); 
-        Taer.display();
-        Diaz.display();
-        Magusib.display();
-        Palermo.display();
-        Mapula.display();	      
-        Conde.display();
-        Candilada.display();
-        Saren.display();
-        Maravilla.display();
-        Servandil.display();        
-        Benoya.display();
-        Apolonio.display();
-        Geguiera.display();
-        Bargaso.display();
+
         Cangmaong.display();
     }
 }
