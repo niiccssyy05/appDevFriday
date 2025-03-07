402e91e295bc5da48de7a3377d74366ec2743b96
josh <mmaravilla@paterostechonologicalcollege.edu.ph>
2025-03-07 19:46:31 +0800

    Fixed Maravilla.java and updated week5.java

--- Maravilla.java
+++ Maravilla.java
@@ -2,7 +2,7 @@ public class Maravilla {
     public static void display() {
         int age = 20; // Replace with user input if necessary
         System.out.println("Hello! This is YourLastName contributing to the project.");
-        System.out.println("My current age is: " + 20);
-        System.out.println("Next year, I will be: " + (20 + 1));
+        System.out.println("My current age is: " + age);
+        System.out.println("Next year, I will be: " + (age+ 1));
     }
 }
