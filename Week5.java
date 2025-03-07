a850d33ac9b6d88f6d5c7ae8b87ef2a3ade292c1
juntabiosGit <165021843+juntabiosGit@users.noreply.github.com>
2025-03-07 19:26:14 +0800

    FINAL COPY

--- Week5.java
+++ Week5.java
@@ -1,8 +1,10 @@
 public class Week5 {
     public static void main(String[] args) {
         System.out.println("Collaborative Java Project\n");
-
+         System.out.println("WEEK 5\n");
         // Call each student's method
-        Cruz.display(); // Add your method call here
+        // Add your method below
+        // Cruz.display(); 
+        
     }
 }
