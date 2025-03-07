f432e2ba9ee0bdb9efe6eff18698fa1a38da678e
Ac092102 <ajcangmaong@paterostechnologicalcollege.edu.ph>
2025-03-08 02:20:37 +0800

    Update Week5.java

--- Week5.java
+++ Week5.java
@@ -1,13 +1,15 @@
 public class Week5 {
     public static void main(String[] args) {
-        System.out.println("Collaborative Java Project\n")
+        System.out.println("Collaborative Java Project\n");
+         System.out.println("WEEK 5\n");
         // Call each student's method
         // Add your method below
         // Cruz.display(); 
         Taer.display();
         Diaz.display();
         Magusib.display();
-        Palermo.display();	      
+        Palermo.display();
+    	Mapula.display();	      
         Conde.display();
         Candilada.display();
     	Saren.display();
