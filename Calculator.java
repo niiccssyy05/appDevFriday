8dce788e6120432eb2ddfff47a32ce51b9fab5fe
josh <mmaravilla@paterostechonologicalcollege.edu.ph>
2025-03-14 19:20:15 +0800

    Group 1: Added addition method (Villoso, Maravilla, Saren, Bolando, Agustin, Eugenio, Oracion, Candilada, Bobis, Geguiera, Dionisio)

--- Calculator.java
+++ Calculator.java
@@ -6,9 +6,9 @@ public class Calculator {
     }
     // Other groups will add their methods here
 
-     // Group 10: Equality Check (Members: Darryl Candilada, Villoso, Maravilla, 
-    // Saren, Bolando, Agustin, Eugenio, Oracion, Candilada, Bobis, Geguiera, Dionisio)
-     public static boolean isEqual(int a, int b) {
+    // Group 10: Equality Check (Members: Candilada, Villoso, Maravilla, 
+    // Saren, Bolando, Agustin, Eugenio, Oracion, Bobis, Geguiera, Dionisio)
+    public static boolean isEqual(int a, int b) {
         return a == b;
-     }
+    }
 }
--- GroupWork.java
+++ GroupWork.java
@@ -1,13 +1,14 @@
-//GroupWork.java
+// GroupWork.java
 import java.util.Scanner;
-public class GroupWork   {
+
+public class GroupWork {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter first number: ");
         int num1 = scanner.nextInt();
         System.out.print("Enter second number: ");
         int num2 = scanner.nextInt();
-       
+
         // Calling methods from Calculator.java
         System.out.println("Sum: " + Calculator.add(num1, num2));  // Group 1's method
         System.out.println("Are they equal? " + Calculator.isEqual(num1, num2));  // Group 10's method
