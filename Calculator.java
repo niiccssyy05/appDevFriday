b68bc7ab1e13b854f1182066c96e22bf3df79521
josh <mmaravilla@paterostechonologicalcollege.edu.ph>
2025-03-14 19:12:57 +0800

    Group 10: Added addition method ( Darryl Candilada, Villoso, Maravilla,      // Saren, Bolando, Agustin, Eugenio, Oracion, Candilada, Bobis, Geguiera, Dionisio)

--- Calculator.java
+++ Calculator.java
@@ -6,4 +6,9 @@ public class Calculator {
     }
     // Other groups will add their methods here
 
+     // Group 10: Equality Check (Members: Darryl Candilada, Villoso, Maravilla, 
+    // Saren, Bolando, Agustin, Eugenio, Oracion, Candilada, Bobis, Geguiera, Dionisio)
+     public static boolean isEqual(int a, int b) {
+        return a == b;
+     }
 }
--- GroupWork.java
+++ GroupWork.java
@@ -10,5 +10,7 @@ public class GroupWork   {
        
         // Calling methods from Calculator.java
         System.out.println("Sum: " + Calculator.add(num1, num2));  // Group 1's method
+        System.out.println("Are they equal? " + Calculator.isEqual(num1, num2));  // Group 10's method
+
     }
 }
