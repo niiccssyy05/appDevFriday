9e56d18fe2b2653cfb63893f1d7261115a0727d8
niiccssyy05 <tesalonicasurilla9@gmail.com>
2025-04-07 03:11:09 +0800

    Update Activity1.java

--- Activity1.java
+++ Activity1.java
@@ -1,1 +1,8 @@
-System.out.println("Tesalonica A. Surilla - BSIT");
+public class Activity1 {
+    public static void main(String[] args) {
+        System.out.println("List of Students:Tesalonica A. Surilla");
+   
+        // Collaborators: Add your names below using the format:
+        // System.out.println("Tesalonica A. Surilla - BSIT 2J");
+    }
+}
